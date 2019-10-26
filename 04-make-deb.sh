1e0680a980d9166abae8350088b326ca9a622206
psi <ryo.hirafuji@link-u.co.jp>
2019-10-26 18:20:35 +0900

    add codename

--- 04-make-deb.sh
+++ 04-make-deb.sh
@@ -7,6 +7,6 @@ yes "" | sudo -k checkinstall \
   --pkgsource="https://github.com/nginx/nginx" \
   --maintainer="ryo.hirafuji@link-u.co.jp" \
   --pkgarch=$(dpkg --print-architecture) \
-  --requires="libxml2 \(\>= 2.9.4\), libxslt1.1 \(\>= 1.1.29\), libgd3 \(\>= 2.2.5\), libgeoip1 \(\>= 1.6.12-1\), libc6 \(\>= 2.28\)" \
+  --requires="libxml2 \(\>= 2.9.4\), libxslt1.1 \(\>= 1.1.29\), libgd3 \(\>= 2.2.5\), libgeoip1 \(\>= 1.6.12-1\), libc6 \(\>= 2.27\)" \
     make install
 
--- all.sh
+++ all.sh
@@ -6,4 +6,5 @@ env --chdir .            bash -eu 01-unpack.sh
 env --chdir ${nginx_dir} bash ../02-configure.sh
 env --chdir ${nginx_dir} bash ../03-build.sh
 env --chdir ${nginx_dir} bash ../04-make-deb.sh
-cp ${nginx_dir}/*.deb .
+cp "${nginx_dir}/nginx_1.17.5-1_amd64.deb" \
+   "./nginx_1.17.5-1+$(lsb_release -c -s)_amd64.deb"
