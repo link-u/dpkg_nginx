dd0dbb64fdf23273af7ffe1a2228b593a0fc3d7a
psi <ryo.hirafuji@link-u.co.jp>
2019-11-12 14:42:41 +0900

    rm examples

--- nginx-1.17.5/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(nginx):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="nginx" command="/usr/bin/nginx"
--- nginx-1.17.5/debian/nginx.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the nginx package
-#
-0 4	* * *	root	[ -x /usr/bin/nginx_maintenance ] && /usr/bin/nginx_maintenance
