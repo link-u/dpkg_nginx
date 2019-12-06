7a7762fcda19addbd199c4cb3f94cc4dd5181bc0
y-hashida <yoshimasa.hashida@link-u.co.jp>
2019-12-06 22:51:47 +0900

    nginx のファイル配置箇所を変更

--- all.sh
+++ all.sh
@@ -6,4 +6,4 @@ nginx_dir="nginx-1.17.5"
 env --chdir openssl tar xvf openssl-1.0.2t.tar.gz
 env --chdir pcre    tar xvf pcre-8.43.tar.gz
 env --chdir zlib    tar xvf zlib-1.2.11.tar.gz
-env --chdir ${nginx_dir} dpkg-buildpackage -us -uc
+env --chdir nginx/${nginx_dir} dpkg-buildpackage -us -uc
