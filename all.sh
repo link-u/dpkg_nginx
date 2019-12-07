b0375505013f28c78e23fd91c66af99724cfdc47
y-hashida <yoshimasa.hashida@link-u.co.jp>
2019-12-07 15:02:31 +0900

    この行はしばらく必要なさそうなので削除.

--- all.sh
+++ all.sh
@@ -2,7 +2,6 @@
 
 nginx_dir="nginx-1.17.5"
 
-#tar xvf nginx-1.17.5.tar.gz
 env --chdir openssl tar xvf openssl-1.0.2t.tar.gz
 env --chdir pcre    tar xvf pcre-8.43.tar.gz
 env --chdir zlib    tar xvf zlib-1.2.11.tar.gz
