b6ce0b50b78f17d8d6d3b6f140a04e2acb3145a7
y-hashida <yoshimasa.hashida@link-u.co.jp>
2020-01-30 17:41:39 +0900

    update: deb ファイル生成用の GitHub action の更新

--- all.sh
+++ all.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 
-nginx_dir="nginx-1.17.7"
+nginx_dir="$(ls -1vd nginx/*/ | sed 's/\/$//g' | tail -n 1)"; \
+nginx_dir="$(basename ${nginx_dir})"
 
 env --chdir openssl tar xvf openssl-1.0.2t.tar.gz
 env --chdir pcre    tar xvf pcre-8.43.tar.gz
 env --chdir zlib    tar xvf zlib-1.2.11.tar.gz
-env --chdir nginx/${nginx_dir} dpkg-buildpackage -us -uc
+env --chdir nginx/${nginx_dir} dpkg-buildpackage -us -uc -j"$(nproc)"
