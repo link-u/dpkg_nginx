736c5f51fdaef0560ea94c3ef4ff1e1723a0823b
y-hashida <yoshimasa.hashida@link-u.co.jp>
2020-01-30 17:59:05 +0900

    update: build-debian-package.yml の更新

--- all.sh
+++ all.sh
@@ -1,9 +1,8 @@
 #! /bin/bash
 
-nginx_dir="$(ls -1vd nginx/*/ | sed 's/\/$//g' | tail -n 1)"; \
-nginx_dir="$(basename ${nginx_dir})"
+nginx_dir="$(ls -1vd nginx/*/ | sed 's/\/$//g' | tail -n 1)";
 
 env --chdir openssl tar xvf openssl-1.0.2t.tar.gz
 env --chdir pcre    tar xvf pcre-8.43.tar.gz
 env --chdir zlib    tar xvf zlib-1.2.11.tar.gz
-env --chdir nginx/${nginx_dir} dpkg-buildpackage -us -uc -j"$(nproc)"
+env --chdir "${nginx_dir}" dpkg-buildpackage -us -uc -j"$(nproc)"
