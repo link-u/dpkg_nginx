1789dacb2f28147aa5e469ea66386e4a51ee012f
y-hashida <yoshimasa.hashida@link-u.co.jp>
2020-01-31 15:18:58 +0900

    update: debian ディレクトリの移動. 不要なファイルの削除.
    - 可能な限り nginx のソースディレクトリは解凍した直後のままで git リポジトリ上に置いておきたかったので debian ディレクトリの場所を変更した.
      それに伴って, ビルドに時に使用する all.sh に debian ディレクトリをコピーさせる処理を追記.
    
    - ビルド時に dh_make を実行させることで, nginx_*.**.*.orig.tar.xz ファイルを自動生成させるようにした.
      したがって, git リポジトリ上で管理する必要がないと判断し削除した.

--- all.sh
+++ all.sh
@@ -1,8 +1,14 @@
 #! /bin/bash
 
-nginx_dir="$(ls -1vd nginx/*/ | sed 's/\/$//g' | tail -n 1)";
+## nginx のソースディレクトリ名の取得
+nginx_src_dir=$(basename "$(ls -1vd nginx/nginx-*.tar.gz | tail -n 1)" .tar.gz);
 
-env --chdir openssl tar xvf openssl-1.0.2t.tar.gz
-env --chdir pcre    tar xvf pcre-8.43.tar.gz
-env --chdir zlib    tar xvf zlib-1.2.11.tar.gz
-env --chdir "${nginx_dir}" dpkg-buildpackage -us -uc -j"$(nproc)"
+## 依存するパッケージの解凍
+env --chdir openssl tar xvf openssl-1.0.2t.tar.gz;
+env --chdir pcre    tar xvf pcre-8.43.tar.gz;
+env --chdir zlib    tar xvf zlib-1.2.11.tar.gz;
+
+## debian package のビルド
+env --chdir nginx   cp -r debian -t "${nginx_src_dir}";
+env --chdir "nginx/${nginx_src_dir}" dh_make --single --createorig --yes;
+env --chdir "nginx/${nginx_src_dir}" dpkg-buildpackage -us -uc -j"$(nproc)";
