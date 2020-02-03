9a966712a9f80c304f8628fe3112b1d65043d9aa
y-hashida <yoshimasa.hashida@link-u.co.jp>
2020-02-03 18:19:13 +0900

    update: deb パッケージ作成時の changelog の管理をやめるようにした.
    changelog_template から簡易的な changelog を自動生成するようにした.
    この changelog には update の情報などは特に書かず, GitHub のリリースノートを参考にしてもらうスタンスをとることにする.

---
+++ scripts/build.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+set -eu
+
+## git リポジトリ上の root のパスを取得
+root_dir=$(env --chdir "$(dirname $0)/.." pwd)
+
+## nginx のソースディレクトリ名の取得
+nginx_src_dir=$(basename "$(ls -1vd ${root_dir}/nginx/nginx-*.tar.gz | tail -n 1)" .tar.gz);
+
+## 依存するパッケージの解凍
+env --chdir "${root_dir}/openssl" tar xvf openssl-1.0.2t.tar.gz;
+env --chdir "${root_dir}/pcre"    tar xvf pcre-8.43.tar.gz;
+env --chdir "${root_dir}/zlib"    tar xvf zlib-1.2.11.tar.gz;
+
+## debian package のビルド
+env --chdir "${root_dir}/nginx"   cp -r debian -t "${nginx_src_dir}";
+echo "${root_dir}/nginx/${nginx_src_dir}"
+env --chdir "${root_dir}/nginx/${nginx_src_dir}" dh_make --single --createorig --yes || true;
+env --chdir "${root_dir}/nginx/${nginx_src_dir}" dpkg-buildpackage -us -uc -j"$(nproc)";
