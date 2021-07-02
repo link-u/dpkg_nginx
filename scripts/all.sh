4144b48de4b54fd1e83eb6002cdfaeb56f747645
psi <ryo.hirafuji@link-u.co.jp>
2021-07-02 13:55:34 +0900

    script調整

--- scripts/all.sh
+++ scripts/all.sh
@@ -3,14 +3,17 @@
 set -eu
 
 ## git リポジトリ上の root のパスを取得
-scripts_dir=$(readlink -f $(cd $(dirname $0) && pwd))
+root_dir="$(cd "$(dirname "$(readlink -f "$0")")" && cd .. && pwd)"
+
+cd "${root_dir}" && exit -1
 
 ## ビルド時に必要なパッケージのインストール
-env --chdir="${scripts_dir}/../nginx" \
+env --chdir="${root_dir}/nginx" \
   mk-build-deps --install --remove \
   --tool='apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends --yes' \
   debian/control
 
 ## deb ファイルのビルド
-bash "${scripts_dir}/create_changelog.sh"
-bash "${scripts_dir}/build.sh"
+bash "scripts/create_changelog.sh"
+bash "scripts/build.sh"
+
--- scripts/build.sh
+++ scripts/build.sh
@@ -3,15 +3,15 @@
 set -eu
 
 ## git リポジトリ上の root のパスを取得
-root_dir=$(readlink -f $(cd $(dirname $0) && cd .. && pwd))
+root_dir="$(cd "$(dirname $(readlink -f "$0"))" && cd .. && pwd)"
 
 ## nginx のソースディレクトリ名の取得
 nginx_src_dir=$(basename "$(ls -1vd ${root_dir}/nginx/nginx-*.tar.gz | tail -n 1)" .tar.gz);
 
 ## 依存するパッケージの解凍
-env --chdir "${root_dir}/openssl" tar xvf openssl-1.1.1k.tar.gz;
-env --chdir "${root_dir}/pcre"    tar xvf pcre-8.45.tar.gz;
-env --chdir "${root_dir}/zlib"    tar xvf zlib-1.2.11.tar.gz;
+env --chdir "${root_dir}/openssl" tar xvf "openssl-1.1.1k.tar.gz"
+env --chdir "${root_dir}/pcre"    tar xvf "pcre-8.45.tar.gz"
+env --chdir "${root_dir}/zlib"    tar xvf "zlib-1.2.11.tar.gz"
 
 ## debian package のビルド
 env --chdir "${root_dir}/nginx"   cp -r debian -t "${nginx_src_dir}";
